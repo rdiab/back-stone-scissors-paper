55c2947da66ac4f6546c16c852883b750d0239f6
Ruben Diab Martinez <rdiab@mercadona.es>
2024-06-25 20:55:12 +0200

    feat: initial archetype

---
+++ boot/src/test/java/com/games/back/stone/scissors/paper/back/stone/scissors/paper/BackStoneScissorsPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.games.back.stone.scissors.paper.back.stone.scissors.paper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStoneScissorsPaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
